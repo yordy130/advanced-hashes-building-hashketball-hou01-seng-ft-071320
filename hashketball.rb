28f499299d9577c19851996d341522859d6d8b09
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-07 01:39:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,24 +4,36 @@ def game_hash
   empty_hash = {
     :home => {
       :team_name => #string of team_name
-      :colors =>
-      :players =>
+      :colors => [#array of strings that are team_colors
+      ]
+      :players => [{#AOH of player_stats
+      :player_name => #value_input,
+      :number => #value_input ,
+      :shoe => #value_input ,
+      :points => #value_input,
+      :rebounds => #value_input,
+      :assists => #value_input,
+      :steals => #value_input,
+      :blocks => #value_input,
+      :slam_dunks => #value_input,
+      #this is the end of the array of hashes for the players key
+      }]
     }
     :away => {
       :team_name => #string of team_name
       :colors => [#array of strings that are team_colors
       ]
       :players => [{#AOH of player_stats
-      :player_name => ,
-      :number =>
-      :shoe =>
-      :points =>
-      :rebounds =>
-      :assists =>
-      :steals =>
-      :blocks =>
-      :slam_dunks =>
-      
+      :player_name => #value_input,
+      :number => #value_input ,
+      :shoe => #value_input ,
+      :points => #value_input,
+      :rebounds => #value_input,
+      :assists => #value_input,
+      :steals => #value_input,
+      :blocks => #value_input,
+      :slam_dunks => #value_input,
+      #this is the end of the array of hashes for the players key
       }]
     }
   }
