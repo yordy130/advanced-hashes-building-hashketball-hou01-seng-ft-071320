e0197be3f839d2d85bdc8453f86b5bb357982bd4
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-07 02:06:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,6 +11,12 @@ def game_hash
        :player_name => "Alan Anderson",
        :number => 0,
        :shoe => 16,
+       :points => 22,
+       :rebounds => 12,
+       :assists => 12,
+       :steals => 3,
+       :blocks => 1,
+       :slam_dunks => 1
         }
      ]
    },
