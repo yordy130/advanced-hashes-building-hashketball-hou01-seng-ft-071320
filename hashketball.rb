9ccaee5313c24a66f20a9d8f618ad48a47adb2e0
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-07 02:30:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -67,7 +67,46 @@ def game_hash
    
    
    :away => {
-     
+     :team_name => "Charlotte Hornets",
+     :colors => ["Turquoise", "Purple"],
+     :players => [
+       {
+        :player_name => "Jeff Adrien",
+        :number => 4,
+        :shoe => 18,
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2
+       },
+       {
+        :player_name => "Bismack Biyombo",
+        :number => 0,
+        :shoe => ,16
+        :points => 12,
+        :rebounds => 4,
+        :assists => 7,
+        :steals => 22,
+        :blocks => 15,
+        :slam_dunks => 10
+       },
+       {
+        :player_name => "DeSagna Diop",
+        :number => 2,
+        :shoe => 14,
+        :points => 24,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5,
+        :slam_dunks => 5
+       },
+       {
+         
+       },
+      ]
    }
  }
 end
