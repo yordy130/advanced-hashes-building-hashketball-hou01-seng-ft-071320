a6c4dc3d3b8850b50554b0e2438ba4caa6e2a1fb
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-07 01:42:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,39 +4,6 @@ require 'pry'
 def game_hash
   #top level
   {
-    :home => {
-      :team_name => Brooklyn Nets #string of team_name
-      :colors => ["Black", "White"#array of strings that are team_colors
-      ]
-      :players => [{#AOH of player_stats
-      :player_name => #value_input,
-      :number => #value_input ,
-      :shoe => #value_input ,
-      :points => #value_input,
-      :rebounds => #value_input,
-      :assists => #value_input,
-      :steals => #value_input,
-      :blocks => #value_input,
-      :slam_dunks => #value_input,
-      #this is the end of the array of hashes for the players key
-      }]
-    }
-    :away => {
-      :team_name => #string of team_name
-      :colors => [#array of strings that are team_colors
-      ]
-      :players => [{#AOH of player_stats
-      :player_name => #value_input,
-      :number => #value_input ,
-      :shoe => #value_input ,
-      :points => #value_input,
-      :rebounds => #value_input,
-      :assists => #value_input,
-      :steals => #value_input,
-      :blocks => #value_input,
-      :slam_dunks => #value_input,
-      #this is the end of the array of hashes for the players key
-      }]
-    }
+    
   }
 end
